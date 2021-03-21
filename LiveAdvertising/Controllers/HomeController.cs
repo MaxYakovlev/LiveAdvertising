62ff30e1e2a4d223c04500b0171b825fa73a420f
MaxYakovlev <y.m.s.21@mail.ru>
2021-03-21 05:15:23 +0300

    show active streams on main page

--- LiveAdvertising/Controllers/HomeController.cs
+++ LiveAdvertising/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LiveAdvertising.Models;
+using LiveAdvertising.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +10,23 @@ using System.Threading.Tasks;
 namespace LiveAdvertising.Controllers
 {
     public class HomeController : Controller
-    {        
-        public IActionResult Index()
+    {
+        private readonly ApplicationContext context;
+
+        public HomeController(ApplicationContext context)
         {
-            return View();
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<Stream> streams = await context.Streams
+                .Where(x => x.isActive == true)
+                .Include(x => x.Shop)
+                .ToListAsync();
+
+            return View(streams);
         }
     }
 }
